7bec52d0564e0e6efaa79a4466969a3533b8d511
boki <lsb530@naver.com>
2024-04-08 00:38:57 +0900

    PessimisticLock

--- src/main/java/com/boki/stock/repository/StockRepository.java
+++ src/main/java/com/boki/stock/repository/StockRepository.java
@@ -1,10 +1,15 @@
 package com.boki.stock.repository;
 
 import com.boki.stock.domain.Stock;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
-
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select s from Stock s where s.id = :id")
+    Stock findByIdWithPessimisticLock(Long id);
 }
---
+++ src/main/java/com/boki/stock/service/PessimisticLockStockService.java
@@ -0,0 +1,24 @@
+package com.boki.stock.service;
+
+import com.boki.stock.domain.Stock;
+import com.boki.stock.repository.StockRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PessimisticLockStockService {
+     private final StockRepository stockRepository;
+
+    public PessimisticLockStockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        Stock stock = stockRepository.findByIdWithPessimisticLock(id);
+
+        stock.decrease(quantity);
+
+        stockRepository.save(stock);
+    }
+}
