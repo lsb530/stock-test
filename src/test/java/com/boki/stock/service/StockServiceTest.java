895baba7065287ab9c48c3fe75be0af5e51ad41e
boki <lsb530@naver.com>
2024-04-08 00:20:29 +0900

    실패하는 테스트

--- src/test/java/com/boki/stock/service/StockServiceTest.java
+++ src/test/java/com/boki/stock/service/StockServiceTest.java
@@ -9,6 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -42,5 +46,29 @@ class StockServiceTest {
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
         assertEquals(99, stock.getQuantity());
+    }
+
+    @DisplayName("동시에 100개 요청")
+    @Test
+    public void 동시에_100개_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        // 100 ( 1 * 100) = 0
+        assertEquals(0, stock.getQuantity());
     }
 }
