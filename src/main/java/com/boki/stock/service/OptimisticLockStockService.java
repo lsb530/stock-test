d8b4bac8c2962c6ccff13c168c5ca1be02701a2a
boki <lsb530@naver.com>
2024-04-08 00:50:01 +0900

    OptimisticLock

---
+++ src/main/java/com/boki/stock/service/OptimisticLockStockService.java
@@ -0,0 +1,24 @@
+package com.boki.stock.service;
+
+import com.boki.stock.domain.Stock;
+import com.boki.stock.repository.StockRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class OptimisticLockStockService {
+    private final StockRepository stockRepository;
+
+    public OptimisticLockStockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        Stock stock = stockRepository.findByIdWithOptimisticLock(id);
+
+        stock.decrease(quantity);
+
+        stockRepository.save(stock);
+    }
+}
---
+++ src/test/java/com/boki/stock/facade/OptimisticLockStockFacadeTest.java
@@ -0,0 +1,69 @@
+package com.boki.stock.facade;
+
+import com.boki.stock.domain.Stock;
+import com.boki.stock.repository.StockRepository;
+import com.boki.stock.service.PessimisticLockStockService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class OptimisticLockStockFacadeTest {
+
+    @Autowired
+    // private StockService stockService;
+    private OptimisticLockStockFacade optimisticLockStockFacade;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void before() {
+        Stock stock = new Stock.StockBuilder()
+                .productId(1L)
+                .quantity(100L)
+                .build();
+        stockRepository.saveAndFlush(stock);
+    }
+
+    @AfterEach
+    public void after() {
+        stockRepository.deleteAll();
+    }
+
+    @DisplayName("동시에 100개 요청")
+    @Test
+    public void 동시에_100개_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    optimisticLockStockFacade.decrease(1L, 1L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        // 100 ( 1 * 100) = 0
+        assertEquals(0, stock.getQuantity());
+    }
+
+}
